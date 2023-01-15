6c38d6e1b9b66c85b6e7f40bf7f7beb4a5b13aa1
sbaranchuk <sbaranchuk@protonmail.com>
2023-01-15 19:31:52 +0300

    initial project settings

--- Dz14/View/PhotosViewController.swift
+++ Dz14/View/PhotosViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  PhotosViewController.swift
 //  Dz14
 //
 //  Created by Admin on 15/01/2023.
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class PhotosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
-
