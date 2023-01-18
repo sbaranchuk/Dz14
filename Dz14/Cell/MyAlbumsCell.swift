4395595e10cfbe6bf069659ab6b34fe5ce3ba061
sbaranchuk <sbaranchuk@protonmail.com>
2023-01-18 14:54:04 +0300

    Customized the appearance of the Album section

--- Dz14/Cell/MyAlbumsCell.swift
+++ Dz14/Cell/MyAlbumsCell.swift
@@ -11,11 +11,68 @@ class MyAlbumsCell: UICollectionViewCell {
 
     static let identifier = "MyAlbumsCell"
 
+    // MARK: - Outlets
+
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "IMG_6827")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.contentMode = .center
+
+        imageView.layer.cornerRadius = 5
+        return imageView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Recents"
+        label.font = label.font.withSize(14)
+        return label
+    }()
+
+    private lazy var counterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "129"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.textColor = UIColor.systemGray
+        return label
+    }()
+
+    // MARK: - Initializer
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupHierarchy()
+        setupLayout()
     }
 
     required init?(coder: NSCoder) {
         fatalError("ERROR")
+    }
+
+    // MARK: - Setups
+
+    private func setupHierarchy() {
+        addSubview(imageView)
+        addSubview(titleLabel)
+        addSubview(counterLabel)
+    }
+
+    private func setupLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.left.right.equalTo(contentView)
+            make.height.width.equalTo(170)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(2)
+            make.left.equalTo(imageView.snp.left)
+        }
+
+        counterLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.left.equalTo(imageView.snp.left)
+        }
     }
 }
--- Dz14/View/AlbumsTabBarViewController.swift
+++ Dz14/View/AlbumsTabBarViewController.swift
@@ -54,21 +54,21 @@ class AlbumsTabBarViewController: UIViewController {
 
             switch sectionIndex {
             case 0:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95),
                                                       heightDimension: .fractionalHeight(0.5))
 
                 let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-                layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0)
+                layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
 
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
-                                                       heightDimension: .fractionalHeight(0.5))
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.46),
+                                                       heightDimension: .fractionalHeight(0.53))
 
                 let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: layoutItem, count: 2)
-                layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+                layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
 
                 let sectionLayout = NSCollectionLayoutSection(group: layoutGroup)
-                sectionLayout.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
-                sectionLayout.orthogonalScrollingBehavior = .continuous
+                sectionLayout.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 5)
+                sectionLayout.orthogonalScrollingBehavior = .groupPaging
 
                 return sectionLayout
             default:
@@ -105,11 +105,12 @@ extension AlbumsTabBarViewController: UICollectionViewDelegate, UICollectionView
         1
     }
 
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         switch indexPath.section {
         case 0:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyAlbumsCell.identifier, for: indexPath)
-            cell.backgroundColor = .systemGreen
+            cell.backgroundColor = .clear
             return cell
         case 1:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyAlbumsCell.identifier, for: indexPath)
